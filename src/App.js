403fd924e389d7a280dbd90ce8295d0c626c0e63
Rony Castillo <10284072+Rocastar@users.noreply.github.com>
2025-02-06 01:55:49 +0000

    mi primer react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Presentation from './components/resume';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Presentation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const Presentation = () => {
+  return (
+    <div>
+      <h1>Hola, soy un componente de React</h1>
+      <p>Este es un componente funcional básico.</p>
+    </div>
+  );
+};
+
+export default Presentation;
